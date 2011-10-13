da4629b2a959674a5ba7b345dc6fdbd4c9865144
David Demaree <ddemaree@gmail.com>
2011-10-13 16:52:21 -0500

    Added aliases for Merb's version of Hash#extract!

--- merb-core/lib/merb-core/core_ext/hash.rb
+++ merb-core/lib/merb-core/core_ext/hash.rb
@@ -9,6 +9,17 @@ class Hash
   #
   # :api: public
   def extract!(*args)
+    merb_extract_values!
+  end
+
+  # These methods are here to distinguish from ActiveSupport's
+  # extract! method, which behaves differently.
+
+  def merb_extract!(*args)
+    merb_extract_values!
+  end
+
+  def merb_extract_values!(*args)
     args.map do |arg|
       self.delete(arg)
     end
